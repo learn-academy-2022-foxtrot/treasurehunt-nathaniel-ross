b70af5779d37cc89e6bc884f5ee89064d903f542
nathaniel-ross <nathaniel.rossloyo@gmail.com>
2022-09-26 11:23:13 -0700

    select the winning square the question mark will become a treasure emoji and if I select the losing square the question mark will become a bomb emoji

--- src/App.js
+++ src/App.js
@@ -14,12 +14,26 @@ const App = () => {
     "?",
     "?"
   ])
+  const [treasureLocation, setTreasureLocation] = useState(Math.floor(Math.random() * board.length))
+  const [bombLocation, setBombLocation] = useState(Math.floor(Math.random() * board.length))
 
   const handleGamePlay = (clickedSquare) => {
+    // makes a copy of the board in state so that we can modify it in this method
     let updateBoard = [...board]
+    if(clickedSquare === treasureLocation) {
+      updateBoard[clickedSquare] = "💰"
+      setBoard(updateBoard)
+    } else if(clickedSquare === bombLocation) {
+      updateBoard[clickedSquare] = "💣"
+      setBoard(updateBoard)
+    } else {
     updateBoard[clickedSquare] = "🌳"
     setBoard(updateBoard)
+    }
   }
+
+  console.log("treasureLocation:", treasureLocation)
+  console.log("bombLocation:", bombLocation)
 
   return (
     <>
